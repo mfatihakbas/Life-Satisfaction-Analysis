d6c5206e562bc78899bd47f982ad74e20f877cc7
Baris34 <46277882+Baris34@users.noreply.github.com>
2024-11-14 01:48:24 +0300

    update

--- database/csv_to_postgresql.py
+++ database/csv_to_postgresql.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # PostgreSQL bağlantı dizesini buraya kendi bilgilerinize göre düzenleyin
-engine = create_engine('postgresql://postgres:460203@localhost:5432/mutluluk_duzeyi')
+engine = create_engine('postgresql://postgres:1234@localhost:5432/mutluluk_verisi')
 
 # CSV dosyasını pandas ile ISO-8859-1 kodlamasıyla okuyun
-df = pd.read_csv(r'C:\Users\fatih\Downloads\mutluluk_verisi.csv', encoding='ISO-8859-1')
+df = pd.read_csv(r'C:\\Users\\Baris\\Documents\\GitHub\\Life-Satisfaction-Analysis\\database\\mutluluk_verisi.csv', encoding='ISO-8859-1')
 
 # Veriyi PostgreSQL'e ekleyin
 df.to_sql('mutluluk_verisi', engine, if_exists='append', index=False)
