b39a1f215880103b7c7e44a220ae8e53ce65ae9b
Baris34 <46277882+Baris34@users.noreply.github.com>
2025-01-02 03:21:51 +0300

    DBChanges

--- database/csv_to_postgresql.py
+++ database/csv_to_postgresql.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 engine = create_engine('postgresql://postgres:1234@localhost:5432/mutluluk_duzeyi')
 
-df = pd.read_csv(r'C:\\Users\Baris\\Documents\\GitHub\\Life-Satisfaction-Analysis\\database\\refah_seviyesi.csv')
+df = pd.read_csv(r'C:\\Users\Baris\\Documents\\GitHub\\Life-Satisfaction-Analysis\\database\\RefahVeMutluluk.csv')
 
-df.to_sql('refah_seviyesi', engine, if_exists='append', index=False)
+df.to_sql('refah_ve_mutluluk', engine, if_exists='append', index=False)
 
 print("Veri başarıyla PostgreSQL'e eklendi.")
